a2b6f2c324a7cb09897cbd6eb019a849314fd135
manoti3 <hillarymanoti0@gmail.com>
2024-03-24 16:13:28 +0300

    completed assignment

---
+++ lib/migrations/versions/e5a5cfcb6e9a_renaming_name_column.py
@@ -0,0 +1,24 @@
+"""renaming name to nick_name
+
+Revision ID: 82d491002ae5
+Revises: 791279dd0760
+Create Date: 2024-03-17 09:54:58.542411
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '82d491002ae5'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
